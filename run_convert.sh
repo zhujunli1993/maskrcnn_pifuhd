8829dfb229c15b05e40aa9b7a54b4cb13df01a39
Zhujun Li <julialee212@gmail.com>
2022-02-06 23:00:28 -0500

    Update run_convert.sh

--- run_convert.sh
+++ run_convert.sh
@@ -1,2 +1,3 @@
-image_dir = 'pifuhd/sample_images'
-python -m apps.simple_test -r 256 --use_rect -i $image_dir
+image_dir='pifuhd/sample_images'
+python -m pifuhd.apps.simple_test -r 256 --use_rect -i $image_dir --ckpt_path pifuhd/checkpoints/pifuhd.pt 
+
