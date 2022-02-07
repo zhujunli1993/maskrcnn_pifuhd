056c3af7344ba508562ad61885b68c4d207a8f12
Zhujun Li <julialee212@gmail.com>
2022-02-07 13:05:27 -0500

    Update maskrcnn_download.sh

--- maskrcnn_download.sh
+++ maskrcnn_download.sh
@@ -1,2 +1,5 @@
 git clone https://github.com/matterport/Mask_RCNN.git
 wget https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
+pip install tensorflow==1.13.1
+pip install keras==2.0.8
+pip install h5py==2.10.0
