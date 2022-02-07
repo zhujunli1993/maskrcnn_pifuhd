c0cd518f5c4b35cf31ab1a1c944fe3109e6cc9a8
Zhujun Li <julialee212@gmail.com>
2022-02-07 13:04:52 -0500

    Update maskrcnn_download.sh

--- maskrcnn_download.sh
+++ maskrcnn_download.sh
@@ -1,1 +1,2 @@
-git clone https://github.com/tensorflow/tpu/
+git clone https://github.com/matterport/Mask_RCNN.git
+wget https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
